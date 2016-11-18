33950f4d8a08473a4bebd6273a425590c0042660
Atnanasi <ys2000pro@gmail.com>
2016-11-18 23:15:30 +0900

    use nkf(EUC-JP)

--- conv-skk.sh
+++ conv-skk.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
-echo "conv-skk.sh 2.1"
+echo "conv-skk.sh 2.2"
 
-#check
+#argument check
 if [ $# != 3 ]; then
-    echo "usage: $0 skk-header.txt msime.dic skk.dic" 1>&2
-    exit 0
+	echo "usage: $0 skk-header.txt msime.dic skk.dic" 1>&2
+	exit 0
 fi
 
-#header
-cat $1 > $3
+#nkf check
+if type nkf > /dev/null 2>&1; then
+	echo "Converting $1+$2 to $3"
+else
+	echo "nkf not found. conv-skk.sh is need nkf."
+	exit 1
+fi
 
-#convert
-cat $2 | sed "s/^\(.\+\)\t\(.\+\)\t\(.\+\)\t\(.\+\)$/\1 \/\2;\3 \4\//" >> $3
+#make tempfile
+tmpfile=`mktemp`
+
+#set header
+cat $1 > $tmpfile
+
+#dict convert
+cat $2 | sed "s/^\(.\+\)\t\(.\+\)\t\(.\+\)\t\(.\+\)$/\1 \/\2;\3 \4\//" >> $tmpfile
+
+#UTF-8 to EUC-JP
+nkf -e $tmpfile > $3
+
+#delete tempfile
+rm $tmpfile
+
+#finished!
+echo "finished."
